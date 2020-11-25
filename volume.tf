6228aa10617d132f114b98ec5234a0e1fc14e0a7
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-25 14:38:22 +0530

    added remote-exec provisioner

--- volume.tf
+++ volume.tf
@@ -11,4 +11,21 @@ resource "aws_volume_attachment" "volume_1_attachment_server_1"{
 	instance_id = aws_instance.server_1.id
 	device_name = "/dev/sdb"
 	skip_destroy = "true"
+
+	provisioner "remote-exec" {
+		connection {
+			type = "ssh"
+			user = "root"
+			password = "thinknyx@123"
+			host = aws_instance.server_1.public_ip
+		}
+		inline = [
+			"yum install -y git",
+			"mkdir /data",
+			"mkfs -t ext4 /dev/xvdb",
+			"mount /dev/xvdb /data",
+			"git clone https://github.com/kul-unisys/sample-java-app.git /data/demo"
+		]
+	}
+	
 }
