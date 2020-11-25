abd4899320447a7c6ba83e76aacc8aa4fac81fa8
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-25 12:37:35 +0530

    print root_devices for EC2 Server

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "server_1_public_ip" {
 	value = "Public IP is ${aws_instance.server_1.public_ip} and DNS is ${aws_instance.server_1.public_dns}" 
 }
+
+output "server_1_root_device" {
+	value = aws_instance.server_1.root_block_device.*.device_name
+}
