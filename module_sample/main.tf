4aec68540080bc98adc2564ce61e62f36da20017
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-26 09:43:17 +0530

    calling module with variable

--- module_sample/main.tf
+++ module_sample/main.tf
@@ -2,8 +2,13 @@ provider "aws"{
     region = "us-east-1"
 }
 
+variable "tagname" {
+    default = "somevalue"
+}
+
 module "create_ec2"{
     source = "./modules/ec2"
+    tagname = var.tagname
 }
 
 output "server_ip" {
