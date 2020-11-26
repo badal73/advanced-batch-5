a7e3ba21b98b429cd7615642549fa47a504e42c1
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-26 10:39:11 +0530

    updated to created EC2 using existing Subnet from the VPC we have created

--- datasource/vpc_details.tf
+++ datasource/vpc_details.tf
@@ -10,3 +10,19 @@ data "aws_vpc" "kul-vpc" {
       values = [var.tagname]
     }
 }
+data "aws_subnet" "public" {
+  vpc_id = data.aws_vpc.kul-vpc.id
+
+  tags = {
+    Name = "Public"
+  }
+}
+resource "aws_instance" "server_1"{
+  ami = "ami-04bf6dcdc9ab498ca"
+  instance_type = "t2.micro"
+  tags = {
+	Name = var.tagname
+  }
+  subnet_id = data.aws_subnet.public.id
+  key_name = "kul-aws-sync"
+}
