00978a9e25990ceca78ff46231f40fe482f732cf
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-27 15:16:22 +0530

    fix

--- manage_kubernetes_with_terraform/service.tf
+++ manage_kubernetes_with_terraform/service.tf
@@ -1,10 +1,11 @@
 resource "kubernetes_service" "kul_service" {
   metadata {
     name = kubernetes_deployment.kul_deploy.metadata.0.name
+    namespace = kubernetes_namespace.kul_ns.metadata.0.name
   }
   spec {
     selector = {
-        client = kubernetes_deployment.kul_deploy.spec.template.metadata.0.labels.client
+        client = kubernetes_deployment.kul_deploy.spec[0].template[0].metadata.0.labels.client
     }
     port {
       port = 80
